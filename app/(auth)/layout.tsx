453ebc0dc57e4fc10badbc09938c1fa5b9e0ade7
Peter Steinberg <bklynpeter@gmail.com>
2023-10-27 18:16:37 -0400

    Integrate Clerk for auth - install - provider - sign-in & sign-up routes & components - NOTE: seems like .env info no longer needed for redirects? - middleware

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+const AuthLayout = ({ children }: Props) => {
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  )
+}
+
+export default AuthLayout
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
       <p className="text-3xl font-bold text-indigo-500">
         This is a protected route
       </p>
+      <UserButton afterSignOutUrl="/" />
     </main>
   )
 }
