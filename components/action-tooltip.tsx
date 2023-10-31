d5e14055f4d7f7bb39b7afc42c20f0c0a03533ea
Peter Steinberg <bklynpeter@gmail.com>
2023-10-31 18:29:26 -0400

    Work on Navigation Sidebar
    - Create NavigationSidebar, NavigationItem, NavigationAction

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import { useEffect, useState } from 'react'
+
+type Props = {
+  label: string
+  children: ReactNode
+  side?: 'top' | 'right' | 'bottom' | 'left'
+  align?: 'start' | 'center' | 'end'
+}
+
+const ActionTooltip = (props: Props) => {
+  const { label, children, side, align } = props
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+    return () => setIsMounted(false)
+  }, [])
+
+  if (!isMounted) return null
+
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export default ActionTooltip
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,54 @@
+import NavigationAction from '@/components/navigation/navigation-action'
+import NavigationItem from '@/components/navigation/navigation-item'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Separator } from '@/components/ui/separator'
+import { currentProfile } from '@/lib/current-profile'
+import prisma from '@/lib/db'
+import { redirect } from 'next/navigation'
+import { ModeToggle } from '../mode-toggle'
+import { UserButton } from '@clerk/nextjs'
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile()
+  if (!profile) return redirect('/')
+  const servers = await prisma.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  })
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] pt-2">
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+
+      <div className="pb-3 mt-auto flex items-centr flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: 'h-[48px] w-[48px]',
+            },
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default NavigationSidebar
